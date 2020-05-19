03387516a20d3e657be3624d60f63e19f74b31de
dqrcodebase <852601818@qq.com>
2020-05-19 17:09:58 +0800

    引入eslint

---
+++ src/layout/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    afadsfas
+    <v-Header />
+  </div>
+</template>
+<script>
+import vHeader from './components/header.vue'
+
+export default {
+  components: {
+    vHeader
+  }
+}
+</script>
