1d684de8968b083f90dd975cc01edc143b50e0ca
dqrcodebase <852601818@qq.com>
2020-08-14 09:34:36 +0800

    解决冲突

--- src/layout/components/header.vue
+++ src/layout/components/header.vue
@@ -1,32 +1,4 @@
 <template>
-<<<<<<< HEAD
-  <el-menu
-    :default-active="activeIndex"
-    class="el-menu-demo"
-    mode="horizontal"
-    @select="handleSelect"
-  >
-    <el-menu-item index="1">处理中心</el-menu-item>
-    <el-submenu index="2">
-      <template slot="title">我的工作台</template>
-      <el-menu-item index="2-1"
-        ><router-link to="/index">index</router-link></el-menu-item
-      >
-      <el-menu-item index="2-2">选项2</el-menu-item>
-      <el-menu-item index="2-3">选项3</el-menu-item>
-      <el-submenu index="2-4">
-        <template slot="title">选项4</template>
-        <el-menu-item index="2-4-1">选项1</el-menu-item>
-        <el-menu-item index="2-4-2">选项2</el-menu-item>
-        <el-menu-item index="2-4-3">选项3</el-menu-item>
-      </el-submenu>
-    </el-submenu>
-    <el-menu-item index="3" disabled>消息中心</el-menu-item>
-    <el-menu-item index="4"
-      ><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item
-    >
-  </el-menu>
-=======
   <el-header>
     <el-menu
       :default-active="activeIndex"
@@ -55,7 +27,6 @@
       >
     </el-menu>
   </el-header>
->>>>>>> dbadfff18b356ef6af545b56901cb58ed45b3078
 </template>
 <script>
 export default {
